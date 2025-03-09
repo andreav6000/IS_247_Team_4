77d49a722006c2d5d71293c8ffb4cc7f0ed42a16
DVU1-UMBC <dvu1@umbc.edu>
2025-03-09 10:22:44 -0400

    I added some inputs, switch statements and a while loop in the main file. It's still a work in progress, but it's a good template on where to start with the other files.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//Work in progress
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world");
-        System.out.print("Something");
+        Scanner scanner = new Scanner(System.in);
+
+        //While statement displays menu options
+        while(true) {
+            System.out.println("1. Add/Search Product");
+            System.out.println("2. Display Inventory");
+            System.out.println("3. Check Low Stock");
+            System.out.println("4. Check Over Stock");
+            System.out.println("5. Check Expired Product");
+            System.out.println("6. Exit");
+
+            //Users navigate inventory using numbers
+            int choice = scanner.nextInt;
+            scanner.nextLine();
+
+            //Switch statements to navigate menu
+            switch(choice){
+                //Add/Search Product
+                case 1:
+                    System.out.print("Enter product name");
+                    String name = scanner.nextLine();
+                    break;
+
+                //For the other cases I guess we can use different files
+                //that each serve a function (i.e. display inventory, check low stock etc.)
+            }
+        }
     }
 }
