69b56616b244bc23b26c89b9c36a8941fdd648e2
DVU1-UMBC <dvu1@umbc.edu>
2025-03-09 10:31:16 -0400

    I added some inputs, switch statements and a while loop in the main file. It's still a work in progress, but it's a good template on where to start with the other files.

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        //While statement displays menu options to look at
+        //While statement displays menu options
         while(true) {
             System.out.println("1. Add/Search Product");
             System.out.println("2. Display Inventory");
